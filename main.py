e5b50c144e815d8ddda1c17d006901fa07725d34
Matthew Nguyen <mnguy1231@gmail.com>
2023-12-31 13:26:08 -0600

    Use one template for all questions

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    return render_template("index.html", num=1)
 
 @app.route("/next-question", methods=["POST"])
 def next_question():
     print(request.get_data())
     print(request.form)
-    return render_template("question.html", num=1)
+    question_num = request.form["question-num"]
+    return render_template("index.html", num=min(int(question_num)+1, 10))
 
 if __name__ == "__main__":
     app.run()
